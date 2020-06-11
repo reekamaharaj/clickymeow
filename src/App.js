b49a8e98ac32f777feb76aa42474b7b05731bb25
Reeka <reekamaharaj@gmail.com>
2020-06-11 14:46:44 -0500

    images added and basic visuals alright. Nothing designed

--- src/App.js
+++ src/App.js
@@ -2,33 +2,26 @@ import React, { Component } from "react";
 import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import pictures from "./pictures.json";
+
 
 class App extends Component {
-    // Setting this.state.friends to the friends json array
+    // Setting this.state.pictures to the pictures json array
     state = {
-        friends,
+        pictures,
     };
 
-    removeFriend = (id) => {
-        // Filter this.state.friends for friends with an id not equal to the id being removed
-        const friends = this.state.friends.filter((friend) => friend.id !== id);
-        // Set this.state.friends equal to the new friends array
-        this.setState({ friends });
-    };
-
-    // Map over this.state.friends and render a FriendCard component for each friend object
+    // Map over this.state.pictures and render a Card component for each picture object
     render() {
         return (
             <Wrapper>
-                <Title>Friends List</Title>
-                {this.state.friends.map((friend) => (
+                <Title>Clicky Meow</Title>
+                {this.state.pictures.map((picture) => (
                     <Card
-                        removeFriend={this.removeFriend}
-                        id={friend.id}
-                        key={friend.id}
-                        name={friend.name}
-                        image={friend.image}
+                        id={picture.id}
+                        key={picture.id}
+                        name={picture.name}
+                        image={picture.image}
                     />
                 ))}
             </Wrapper>
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,26 +1,16 @@
 import React from "react";
 import "./style.css";
 
-function FriendCard(props) {
+
+function Card(props) {
     return (
         <div className="card">
             <div className="img-container">
                 <img alt={props.name} src={props.image} />
             </div>
-            <div className="content">
-                <ul>
-                    <li>
-                        <strong>Name:</strong> {props.name}
-                    </li>
-                </ul>
-            </div>
-            <span
-                onClick={() => props.removeFriend(props.id)}
-                className="remove">
-                𝘅
-            </span>
+            
         </div>
     );
 }
 
-export default FriendCard;
+export default Card;
