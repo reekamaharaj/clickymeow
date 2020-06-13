b4497397e976bf7394557e6c765c45f60890df82
Reeka <reekamaharaj@gmail.com>
2020-06-12 19:56:48 -0500

    working

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         highScore: 0,
         win: 0,
         lose: 0,
-        game: false
+        game: false,
     };
 
     clickedPic = (id) => {
@@ -25,11 +25,15 @@ class App extends Component {
         let lose = this.state.lose;
 
         this.setState({
-            game: true
+            game: true,
         });
 
         if (clickedCat === undefined || clickedCat.length === 0) {
             clickedCat.push(id);
+            this.setState({
+                score: score + 1,
+                pictures: shuffle(pictures)
+            });
             console.log("game started", id);
             //add one to score
             //randomize the images
@@ -37,30 +41,33 @@ class App extends Component {
             this.setState({
                 win: win + 1,
                 score: 0,
-                clickedCat: []
+                clickedCat: [],
+                pictures: shuffle(pictures)
             });
             //restart game, score set to 0 and clear clicked Cat array
-            console.log("win", id)
+            console.log("win", id);
         } else if (clickedCat.includes(id)) {
             //check if img has been clicked before
             this.setState({
                 lose: lose + 1,
                 score: 0,
-                clickedCat: []
+                clickedCat: [],
+                pictures: shuffle(pictures)
             });
             if (score > highScore) {
                 this.setState({
-                    highScore: score
-                })
+                    highScore: score,
+                });
             }
             console.log("lose", id);
             //restart game, score set to 0 and clear clickedCat array
         } else {
             this.setState({
-                score: score + 1
+                score: score + 1,
+                pictures: shuffle(pictures)
             });
             clickedCat.push(id);
-            console.log("plus 1", id)
+            console.log("plus 1", id);
             //randomize pictures
         }
     };
@@ -68,7 +75,12 @@ class App extends Component {
     render() {
         return (
             <div className="container">
-                <Game score={this.state.score} highScore={this.state.highScore} win={this.state.win} lose={this.state.lose}/>
+                <Game
+                    score={this.state.score}
+                    highScore={this.state.highScore}
+                    win={this.state.win}
+                    lose={this.state.lose}
+                />
                 <div className="row">
                     {this.state.pictures.map((picture) => (
                         <Card
@@ -85,6 +97,17 @@ class App extends Component {
     }
 }
 
-export default App;
+function shuffle(array) {
+    let i;
+    let j;
+    let temp;
+    for (i = array.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
 
-//function to randomize image location after something is clicked.
+export default App;
