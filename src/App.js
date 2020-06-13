4f09f5a6c78bfaa1ca0fbb36bd2657314230eb0b
Reeka <reekamaharaj@gmail.com>
2020-06-13 16:30:55 -0500

    score gets to 12 now

--- src/App.js
+++ src/App.js
@@ -34,18 +34,17 @@ class App extends Component {
                 score: score + 1,
                 pictures: shuffle(pictures)
             });
-            console.log("game started", id);
             //add one to score
             //randomize the images
         } else if (clickedCat.length === 12) {
             this.setState({
                 win: win + 1,
                 score: 0,
+                highScore: 12,
                 clickedCat: [],
                 pictures: shuffle(pictures)
             });
             //restart game, score set to 0 and clear clicked Cat array
-            console.log("win", id);
         } else if (clickedCat.includes(id)) {
             //check if img has been clicked before
             this.setState({
@@ -57,9 +56,9 @@ class App extends Component {
             if (score > highScore) {
                 this.setState({
                     highScore: score,
+                    score: 0
                 });
             }
-            console.log("lose", id);
             //restart game, score set to 0 and clear clickedCat array
         } else {
             this.setState({
@@ -67,7 +66,6 @@ class App extends Component {
                 pictures: shuffle(pictures)
             });
             clickedCat.push(id);
-            console.log("plus 1", id);
             //randomize pictures
         }
     };
